0b3aab2e1b59c1a30abf51d07d764cdf2656caa6
cyclop98 <37452543+cyclop98@users.noreply.github.com>
2018-12-14 08:36:53 -0800

    Update paddle.h

--- paddle.h
+++ paddle.h
@@ -3,29 +3,28 @@
 #include "game.h"
 #include "ball.h"
 
-class paddle{
+class paddle
+{
 public:
 	float x, y, h, w, r, g, b;
-	
 	paddle(float x, float y, float h, float w, float r, float g, float b): x(x), y(y), h(h), w(w), r(r), g(g), b(b) {};
-    
-	bool contact(ball* b){
-        float top;
-        float bot;
-        float left;
-        float right;
+	
+	bool ifHit(ball* b){
+        float top, bot, left, right;
 		if (x == 0 && y == 0) {
 			return false;
 		}
+        
 		top = y;
-		bot = y + (1.5 * h);
+		bot = y + (1.0 * h);
 		left = x;
 		right = x + w;
 		return (top <= b->y && bot >= b->y && left <= b->x && right >= b->x);
 	}
-	void draw(){
+	void draw(){ //draws out the paddle that hits the ball
 		glBegin(GL_POLYGON);
 		glColor3d(r, g, b);
+        glColor3d(1, 1, 0);
 		glVertex2f(x, y);
 		glVertex2f(x + w, y);
 		glVertex2f(x + w, y + h);
