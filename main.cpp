69948f03623f7924784ef81f21c039fbd15e7b34
cyclop98 <37452543+cyclop98@users.noreply.github.com>
2018-12-14 08:35:52 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
 #include "game.h"
 
-void vec2_norm(float& x, float &y){
-    float length = sqrt((x * x) + (y * y));
-    if (length != 0.0f) {
-        length = 1.0f / length;
-        x *= length;
-        y *= length;
-    }
-}
-
-string scoring(int x){ //Converts int to string
+string scoring(int x){ //Converts the int value to a string value
     stringstream ss;
     ss << x;
     return ss.str( );
@@ -19,7 +10,7 @@ void Text(float x, float y, const char* text){
     const char *c;
     glRasterPos2f(x, y);
     for (c = text; *c != '\0'; c++) {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *c);
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *c);
     }
 }
 
@@ -34,7 +25,7 @@ void enable2D(int width, int height){
 
 static void key(unsigned char key, int x, int y){
     if (key == ' ' && lives != 0) {
-        ballMain->speed = 5;
+        ballMain->speed = 10;
         gameStart = true;
     }
     if (key == 'r') {  //Restart Level
@@ -68,20 +59,20 @@ void mouseMove(int x, int y){
 
 void mouseButton(int button, int state, int x, int y)
 {
-
+    
 }
 void updateBall(){
     ballMain->x += ballMain->dirX * ballMain->speed;
     ballMain->y += ballMain->dirY * ballMain->speed;
-
-    if (paddleMain->contact(ballMain)) {
+    
+    if (paddleMain->ifHit(ballMain)) {
         float t = ((ballMain->x - paddleMain->x) / paddleMain->w) - 0.5f;
         ballMain->dirY = fabs(ballMain->dirY);
         ballMain->dirX = t;
     }
     int blocksLeft = 0;
     for (vector<bar*>::iterator i = levelMain->bars.begin(); i != levelMain->bars.end(); ++i) {
-        if ((*i)->isHit(ballMain)) {
+        if ((*i)->ifHit(ballMain)) {
             if ((*i)->health != 0) {
                 float t = ((ballMain->x - (*i)->x) / (*i)->w) - 0.5f;
                 float z = ((ballMain->y - (*i)->y) / (*i)->h) - 0.5f;
@@ -90,12 +81,10 @@ void updateBall(){
                 } else {
                     ballMain->dirY = z;
                 }
-
+                
                 ballMain->dirX = t;
                 if (oneHit) {
-                    (*i)->health = 0;
-                } else {
-                    (*i)->health--;
+                   (*i)->health = 0;
                 }
                 if ((*i)->health == 0) {
                     score += 10;
@@ -119,7 +108,7 @@ void updateBall(){
         ballMain->dirX = 0;
         gameStart = false;
     }
-   //If Left/Right/Top/Bottom Wall (in that order) have been hit
+    //If Left/Right/Top/Bottom Wall (in that order) have been hit
     if (ballMain->x < 0) {
         ballMain->dirX = fabs(ballMain->dirX);
     }
@@ -131,7 +120,6 @@ void updateBall(){
     }
     if (ballMain->y < 0) {
         --lives;
-        score -= 20;
         gameStart = false;
         ballMain->speed = 0;
         ballMain->x = (paddleMain->x + (paddleMain->w / 2));
@@ -139,12 +127,11 @@ void updateBall(){
         ballMain->dirY = fabs(ballMain->dirY);
         ballMain->dirX = 0;
     }
-    vec2_norm(ballMain->dirX, ballMain->dirY);
-}
+    }
 void update(int value){
     updateBall();
-    glutTimerFunc(interval, update, 0);
-    glutPostRedisplay();
+   glutTimerFunc(interval, update, 0);
+   glutPostRedisplay();
 }
 void draw(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -155,7 +142,6 @@ void draw(){
         levelMain->draw();
     } else {
         levelMain->bars.clear();
-        gameOverDraw();
         ballMain->speed = 0;
         ballMain->x = (paddleMain->x + (paddleMain->w / 2));
         ballMain->y = (paddleMain->y + paddleMain->h + 4);
@@ -165,10 +151,10 @@ void draw(){
         lives = 3;
         score = 0;
     }
-   //Lives
+    //Lives
     glColor3f(1.0f, 1.0f, 1.0f);
-    Text(14, height - 15,
-             ("Lives: " + scoring(lives) + "                                                                                               Score: " + scoring(score)).c_str());
+    Text(7, height - 15,
+         ("" + scoring(lives) + "                                                                                               Score: " + scoring(score)).c_str());
     glutSwapBuffers();
 }
 
@@ -176,7 +162,7 @@ int main(int argc, char** argv){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(width, height);
-    glutCreateWindow("Brick Breaker");
+    glutCreateWindow("Onblanc Block Destroyer");
     // Register callback functions
     glutKeyboardFunc(key);
     glutMouseFunc(mouseButton);
@@ -187,7 +173,7 @@ int main(int argc, char** argv){
     levelOne();
     enable2D(width, height);
     glColor3f(1.0f, 1.0f, 1.0f);
-
+    
     glutMainLoop();
     return 0;
 }
