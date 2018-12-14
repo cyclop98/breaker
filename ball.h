3337c511e802a17efbb72b32cc30be9f6e37d3a9
cyclop98 <37452543+cyclop98@users.noreply.github.com>
2018-12-14 08:34:39 -0800

    Update ball.h

--- ball.h
+++ ball.h
@@ -2,41 +2,20 @@
 #define BALL_H
 #include "game.h"
 
-class ball
-{
+class ball{
 public:
 	float x, y, dirX, dirY, speed, h, w, radius, r, g, b;
-	ball():
-		x(0), y(0), dirX(0), dirY(0), speed(0), h(0), w(0), radius(0), r(0), g(0), b(0) {};
-
-	ball(float x, float y):
-		x(x), y(y), dirX(0), dirY(0), speed(0), h(0), w(0), radius(0), r(0), g(0), b(0) {};
-
-	ball(float x, float y, float dirX, float dirY, float speed, float h, float w, float radius):
-		x(x), y(y), dirX(dirX), dirY(dirY), speed(speed), h(h), w(w), radius(radius), r(0), g(0), b(0) {};
-
 	ball(float x, float y, float dirX, float dirY, float speed, float h, float w, float radius, float r, float g, float b):
 		x(x), y(y), dirX(dirX), dirY(dirY), speed(speed), h(h), w(w), radius(radius), r(r), g(g), b(b) {};
 
-	~ball() {};
-
-	void drawRect(){
-		glBegin(GL_POLYGON);
-		glColor3d(r, g, b);
-		glVertex2f(x, y);
-		glVertex2f(x + w, y);
-		glVertex2f(x + w, y + h);
-		glVertex2f(x, y + h);
-		glEnd();
-	}
-
-	void draw(){
+    void draw(){ //draws out the circle using opengl
 		float i = 0;
 		float tempX = (x) + radius;
 		float tempY = y - 0.05;
 		while (i < 2 * M_PI) {
 			glBegin(GL_LINES);
 			glColor3d ( r, g, b );
+            glColor3d( 0, 1, 0 );
 			glVertex2d (tempX, tempY);
 			tempX = x + radius * (cos(i));
 			tempY = y + radius * (sin(i));
@@ -45,7 +24,6 @@ public:
 			i += M_PI / 180;
 		}
 	}
-
 };
 
 #endif
