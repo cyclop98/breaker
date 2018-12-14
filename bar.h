d57b4cf52908da1a8cc9b12f24b882a93031b307
cyclop98 <37452543+cyclop98@users.noreply.github.com>
2018-12-14 08:35:20 -0800

    Update bar.h

--- bar.h
+++ bar.h
@@ -2,18 +2,20 @@
 #define BAR_H
 #include "game.h"
 
-class bar{
+class bar
+{
 public:
     float x, y, h, w, r, g, b;
     int health;
     int special;
-    
     bar():
-    x(0), y(0), h(15), w(45), r(0), g(0), b(0),  special(0) {};
+    x(0), y(0), h(15), w(45), r(0), g(0), b(0), health(1), special(0) {};
+    
     bar(float x, float y, float h, float w, int health, int special):
     x(x), y(y), h(h), w(w), r(0), g(1), b(0), health(health), special(special) {};
     
-    bool isHit(ball* b){
+    bool ifHit(ball* b)
+    {
         float top, bot, left, right;
         if (x == 0 && y == 0) {
             return false;
@@ -24,21 +26,14 @@ public:
         right = x + w;
         return (top <= b->y && bot >= b->y && left <= b->x && right >= b->x);
     }
-    void draw(){
+    
+   void draw() //draws out the bricks
+    {
         glBegin(GL_POLYGON);
         glVertex2d(x, y);
         glVertex2d((x + w), y);
         glVertex2d((x + w), (y - h));
         glVertex2d(x, (y - h));
-        glEnd();
-    }
-    
-    void draw(float offsetX, float offsetY){
-        glBegin(GL_POLYGON);
-        glVertex2d(x + offsetX, y + offsetY);
-        glVertex2d((x + offsetX + w), y + offsetY);
-        glVertex2d((x + offsetX + w), (y + offsetY - h));
-        glVertex2d(x + offsetX, (y + offsetY - h));
         glEnd();
     }
 };
