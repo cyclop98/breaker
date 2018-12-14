67a9a9a346bacb9d3f7afd6f6e5be85aa854cb51
cyclop98 <37452543+cyclop98@users.noreply.github.com>
2018-12-14 08:37:13 -0800

    Update level.h

--- level.h
+++ level.h
@@ -7,23 +7,19 @@ class level
 {
 public:
 	vector<bar*> bars;
-	level() {};
-	level(vector<bar*> bars): bars(bars) {};
-	~level() {};
 	
-	
-	void addBar(bar* b){
+    void addBar(bar* b){
 		bars.push_back(b);
 	}
 
-	void draw(){
+	void draw(){ //changes the bricks' color 
 		for (vector<bar*>::iterator i = bars.begin(); i != bars.end(); ++i) {
 			if ((*i)->health != 0) {
-				if ((*i)->health == 5) { //red
-					glColor3d(1, 0, 0);
+				if ((*i)->health == 5) {
+					glColor3d(.2, 1.7, 1.63);
 					(*i)->draw();
-				} else if ((*i)->health == 3) { //green
-					glColor3d(0.46, 0.93, 0);
+				} else if ((*i)->health == 3) { 
+					glColor3d(1.7,.33, 1.93);
 					(*i)->draw();
 				}
 			}
